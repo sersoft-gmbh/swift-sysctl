e21a696e2448b8e5765a56104efecfd7c34dc006
Florian Friedrich <ffried@me.com>
2021-04-27 16:28:29 +0200

    Add Swift 5.4 support

--- Sources/Sysctl/Base/Namespaces.swift
+++ Sources/Sysctl/Base/Namespaces.swift
@@ -36,6 +36,9 @@ public struct SysctlRootNamespace: SysctlNamespace {
 /// Fields are `ExpressibleByStringLiteral`. Thus you can simply use `"yourfieldname"` to create fields.
 @frozen
 public struct SysctlNamedField<Parent: SysctlNamespace, Value: SysctlValue>: Hashable, ExpressibleByStringLiteral {
+    /// See `ExpressibleByStringLiteral`.
+    public typealias StringLiteralType = String
+
     /// The name part of the field.
     @usableFromInline
     let namePart: String
@@ -49,7 +52,7 @@ public struct SysctlNamedField<Parent: SysctlNamespace, Value: SysctlValue>: Has
 
     /// See `ExpressibleByStringLiteral.init(stringLiteral:)`.
     @inlinable
-    public init(stringLiteral value: String) {
+    public init(stringLiteral value: StringLiteralType) {
         self.init(namePart: value)
     }
 }
